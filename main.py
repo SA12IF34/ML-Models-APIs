6c974ca8265b841734c00e84a7140a718e5bbc86
Saif <ayeshsaif367@gmail.com>
2024-09-23 18:22:21 +0300

    Commit

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ app.add_middleware(
     allow_origins=[
         'https://saifchan.online',
         'https://cms.saifchan.online',
-        'https://ml-models.saifchan.online',
-        'http://127.0.0.1:8000'
+        'https://ml-models.saifchan.online'
     ],
     allow_credentials=True,
     allow_headers=['*'],
